3dfe5d99cf797b7cdd643ac9673dd43b7085953e
YI SHAN <Jiraiya812@YIs-MacBook-Pro.local>
2018-04-07 17:38:44 -0400

    added report

---
+++ lib/utils.py
@@ -0,0 +1,11 @@
+import time
+
+def report(msg,tic=None,print_out=True):
+    length = 100
+    report = "\n"+">>>>  "+ msg + " "
+    if tic:
+        report += '-' * (100 - len(report))
+        report +=" TC:{0:.4g}".format(time.time()-tic)
+    report+="\n"
+    if print_out:
+        print(report)
