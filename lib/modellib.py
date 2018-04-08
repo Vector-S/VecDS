92fe7d45023a861c50c302de3c6b0e9ab0d57082
rxnataliezhang <nata.z0101@gmail.com>
2018-04-08 18:24:04 -0400

    build hyperparameter optimizer

--- lib/modellib.py
+++ lib/modellib.py
@@ -14,13 +14,12 @@ def modelfit(model, train_df, label_df, useTrainCV=True, cv_folds=5, early_stopp
         xgb_param = model.get_xgb_params()
         dtrain = xgb.DMatrix(train_df.values, label=label_df.values)
         cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=model.get_params()['n_estimators'], nfold=cv_folds,
-            metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=False)
+            metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=True)
         model.set_params(n_estimators=cvresult.shape[0])
         del dtrain, cvresult
         gc.collect()
 
     # Fit the algorithm on the data
-    # model.fit(train_df, label_df, eval_set=, eval_metric='auc', early_stopping_rounds=early_stopping_rounds)
     model.fit(train_df, label_df, eval_metric='auc')
 
     # Predict training set:
@@ -30,6 +29,7 @@ def modelfit(model, train_df, label_df, useTrainCV=True, cv_folds=5, early_stopp
 
     # Print model report:
     print("\nModel Report")
+    print("Accuracy:", metrics.accuracy_score(label_df.values, train_predictions))
     print("AUC Score (Train):", metrics.roc_auc_score(label_df.values, train_predprob))
 
     ## Plot importance
@@ -43,69 +43,53 @@ def xgb_train(train_df, label_df, useTrainCV=True):
     # Choose all predictors except target & IDcols
 
     model = XGBClassifier(
-                        learning_rate =0.3,
+                        max_depth=5,
+                        learning_rate=0.1,
                         n_estimators=1000,
                         objective= 'binary:logistic',
-                        nthread=8,
-                        max_depth=5,
-                        min_child_weight=0,
-                        subsample=0.9,
-                        colsample_bytree=0.7,
-                        reg_alpha=4,
-                        scale_pos_weight=0.9,
-                        seed=99)
+                        n_jobs=4,
+                        gamma=0.1,
+                        min_child_weight=1,
+                        subsample=0.8, 
+                        colsample_bytree=0.8, 
+                        scale_pos_weight=1, 
+                        random_state=24)
 
     model = modelfit(model, train_df, label_df, useTrainCV)
 
     return model
 
+
+
 def xgb_predict(model,test_df):
     prediction = pd.DataFrame()
-    # prediction['is_attributed'] = model.predict(dtest, ntree_limit=model.best_ntree_limit)
-    prediction['is_attributed'] = model.predict(test_df)
+    prediction['is_attributed'] = model.predict_proba(test_df)[:,1]
     return prediction
 
-# def xgb_train(train_df, label_df, validate=False, params=None):
-#     if not params:
-#         params = {'eta': 0.3,
-#                   'tree_method': "auto",
-#                   'grow_policy': "lossguide",
-#                   'max_leaves': 1400,
-#                   'max_depth': 0,
-#                   'subsample': 0.9,
-#                   'colsample_bytree': 0.7,
-#                   'colsample_bylevel': 0.7,
-#                   'min_child_weight': 0,
-#                   'alpha': 4,
-#                   'objective': 'binary:logistic',
-#                   'scale_pos_weight': 9,
-#                   'eval_metric': 'auc',
-#                   'nthread': 8,
-#                   'random_state': 99,
-#                   'silent': True}
-#     if (validate == True):
-#         # Get 10% of train dataset to use as validation
-#         x1, x2, y1, y2 = train_test_split(train_df, label_df, test_size=0.1, random_state=99)
-#         dtrain = xgb.DMatrix(x1, y1)
-#         dvalid = xgb.DMatrix(x2, y2)
-#         del x1, y1, x2, y2
-#         gc.collect()
-#         watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
-#         model = xgb.train(params, dtrain, 200, watchlist, maximize=True, early_stopping_rounds=25, verbose_eval=5)
-#         del dvalid
-#     else:
-#         dtrain = xgb.DMatrix(train_df, label_df)
-#         del train_df, label_df
-#         gc.collect()
-#         watchlist = [(dtrain, 'train')]
-#         model = xgb.train(params, dtrain, 30, watchlist, maximize=True, verbose_eval=1)
-#     return model
 
 
-# def xgb_predict(model,test_df):
-#     dtest = xgb.DMatrix(test_df)
-#     del test_df
-#     gc.collect()
-#     prediction = pd.DataFrame()
-#     prediction['is_attributed'] = model.predict(dtest, ntree_limit=model.best_ntree_limit)
-#     return prediction
+def opt_model1(train_df, label_df):
+    param_test1 = {'max_depth':list(range(3,10,2)),
+                   'min_child_weight':list(range(1,6,2))
+                   }
+    gsearch1 = GridSearchCV(
+        estimator = XGBClassifier(
+                                max_depth=5,
+                                learning_rate=0.1,
+                                n_estimators=1000,
+                                objective= 'binary:logistic',
+                                n_jobs=4,
+                                gamma=0.1,
+                                min_child_weight=1,
+                                subsample=0.8, 
+                                colsample_bytree=0.8, 
+                                scale_pos_weight=1, 
+                                random_state=24),
+        param_grid = param_test1,     
+        scoring='roc_auc',
+        n_jobs=4,
+        iid=False, 
+        cv=5)
+
+    gsearch1.fit(train_df, label_df)
+    print(gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_)     
