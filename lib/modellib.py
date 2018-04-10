c13ba81cbdfa13719b126008a5fc3dce911336a9
rxnataliezhang <nata.z0101@gmail.com>
2018-04-09 21:04:32 -0400

    clear up in modellib

--- lib/modellib.py
+++ lib/modellib.py
@@ -8,32 +8,10 @@ from matplotlib import pyplot
 import gc
 
 
-def modelfit(model, train_df, label_df, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
-    
-    if useTrainCV:
-        xgb_param = model.get_xgb_params()
-        dtrain = xgb.DMatrix(train_df.values, label=label_df.values)
-        cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=model.get_params()['n_estimators'], nfold=cv_folds,
-            metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=True)
-        model.set_params(n_estimators=cvresult.shape[0])
-        del dtrain, cvresult
-        gc.collect()
 
-    # Fit the algorithm on the data
-    model.fit(train_df, label_df, eval_metric='auc')
+################################################# XGBoost #################################################
 
-    # Predict training set:
-    train_predictions = model.predict(train_df)
-    train_predprob = model.predict_proba(train_df)[:,1]
-   
-
-    # Print model report:
-    print("\nModel Report")
-    print("Accuracy:", metrics.accuracy_score(label_df.values, train_predictions))
-    print("AUC Score (Train):", metrics.roc_auc_score(label_df.values, train_predprob))
-
-
-################################ XGBoost ############################################
+######################### Workflow #########################
 
 def xgb_init(s):
     s.model =  XGBClassifier(learning_rate=0.3,
@@ -49,30 +27,40 @@ def xgb_init(s):
                           seed=99)
 
 
-def xgb_pt_1(s,cv_folds=5, early_stopping_rounds=50):
-    xgb_param = s.model.get_xgb_params()
-    dtrain = xgb.DMatrix(s.train_df.values, label=s.label_df.values)
-    cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=s.model.get_params()['n_estimators'], nfold=cv_folds,
-                      metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=True)
-    s.model.set_params(n_estimators=cvresult.shape[0])
-    del dtrain, cvresult
-    gc.collect()
+def modelfit(model, train_df, label_df, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+    
+    if useTrainCV:
+        xgb_param = model.get_xgb_params()
+        dtrain = xgb.DMatrix(train_df.values, label=label_df.values)
+        cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=model.get_params()['n_estimators'], nfold=cv_folds,
+            metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=True)
+        model.set_params(n_estimators=cvresult.shape[0])
+        del dtrain, cvresult
+        gc.collect()
 
-    model = XGBClassifier(
-                        max_depth=5,
-                        learning_rate=0.1,
-                        n_estimators=1000,
-                        objective= 'binary:logistic',
-                        n_jobs=4,
-                        gamma=0.1,
-                        min_child_weight=1,
-                        subsample=0.8, 
-                        colsample_bytree=0.8, 
-                        scale_pos_weight=1, 
-                        random_state=24)
+    # Fit the algorithm on the data
+    model.fit(train_df, label_df, eval_metric='auc')
+
+def xgb_train(s,eval_metric='auc'):
+    modelfit(s.model, s.train_df, s.label_df)
+    s.train_result = s.model.predict_proba(s.train_df)[:,1]
+    print("\nModel Report")
+    print("AUC Score (Train):", metrics.roc_auc_score(s.label_df.values, s.train_result))
+
+def xgb_save_fi(s):
+    plot_importance(s.model)
+    pyplot.savefig(s.output_path+ s.file_name)
+
+def xgb_test(s,test_df):
+    prediction = pd.DataFrame()
+    prediction['is_attributed'] = s.model.predict_proba(s.test_df)[:,1]
+    s.test_result = prediction
 
 
-def xgb_pt_2(s):
+################# Tune Model Hyperparameters #################
+
+
+def xgb_pt_1(s):
     param_test1 = {'max_depth':list(range(3,10,2)),
                    'min_child_weight':list(range(1,6,2))
                    }
@@ -82,7 +70,7 @@ def xgb_pt_2(s):
                                 learning_rate=0.1,
                                 n_estimators=1000,
                                 objective= 'binary:logistic',
-                                n_jobs=4,
+                                nthread=4,
                                 gamma=0.1,
                                 min_child_weight=1,
                                 subsample=0.8,
@@ -97,20 +85,7 @@ def xgb_pt_2(s):
     gsearch1.fit(s.train_df, s.label_df)
     print(gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_)
 
-def xgb_train(s,eval_metric='auc'):
-    s.model.fit(s.train_df, s.label_df, eval_metric='auc')
-    s.train_result = s.model.predict_proba(s.train_df)[:,1]
-    print("\nModel Report")
-    print("AUC Score (Train):", metrics.roc_auc_score(s.label_df.values, s.train_result))
 
-def xgb_save_fi(s):
-    plot_importance(s.model)
-    pyplot.savefig(s.ouput_path+ s.file_name)
-
-def xgb_test(s,test_df):
-    prediction = pd.DataFrame()
-    prediction['is_attributed'] = s.model.predict_proba(s.test_df)[:,1]
-    s.test_result = prediction
 
 
 ################################ LightGBM ############################################
