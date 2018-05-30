9311cc0faac1a6470e4780f3b220096e1316dd92
Vector-S <jiraiya812@gmail.com>
2018-05-30 00:14:56 -0400

    fix smartlog indentation

--- veclib/smartlog.py
+++ veclib/smartlog.py
@@ -18,17 +18,21 @@ class SmartLog:
 
 
 
-def send_log(message):
-	"""
-	send log string as a message to the phone number. need to register twilio before use
-	Args:
-		message: message to be sent
-	Returns:
-		no return
-	"""
-	account_sid = "ACa6120ed7fd410c4b56030efa0ba9d6a2" # Your Account SID from www.twilio.com/console
-	auth_token	= "8e8f6a573d9b868b8564ee3989996f01"	# Your Auth Token from www.twilio.com/console
-	client = Client(account_sid, auth_token)
-	message = client.messages.create(body=message,
-		to="+15197213077",		# Replace with your phone number
-		from_="+12268871473") # Replace with your Twilio number
+def send_message(msg):
+    """
+    send log string as a message to the phone number. need to register twilio before use
+    Args:
+        msg: message to be sent
+    Returns:
+        no return
+    """
+    account_sid = "ACa6120ed7fd410c4b56030efa0ba9d6a2" # Your Account SID from www.twilio.com/console
+    auth_token	= "8e8f6a573d9b868b8564ee3989996f01"	# Your Auth Token from www.twilio.com/console
+    client = Client(account_sid, auth_token)
+    msg = client.messages.create(body=msg,
+                                 to="+15197213077",  # Replace with your phone number
+                                 from_="+12268871473") # Replace with your Twilio number
+
+
+if __name__ == "__main__":
+    send_message("Test")
